68cb18ac55efe338a167ecedfd8f32aff19f51e9
Long Calamia <lucky93933@csu.fullerton.edu>
2023-03-04 09:43:34 -0800

    fixed issues  with player image file

--- character.py
+++ character.py
@@ -3,9 +3,39 @@ from settings import *
 
 
 class Character:
-    def __init__(self, x, y):
+    def __init__(self, x, y, screen,  image):
         self.x = x
         self.y = y
+        self.screen = screen
+        self.width = 20
+        self.height = 22
+        self.image = pygame.image.load(image).convert_alpha()
+
+        self.frames = [pygame.Rect(self.x, self.y, self.width, self.height)
+                       for x in range(0, self.image.get_width(), 19)]
+
+        self.current_frame = 0
+        self.rect = self.frames[self.current_frame]
+
+        self.rect.x = x
+        self.rect.y = y
+
+        self.speed = 100
+
+        self. last_update = pygame.time.get_ticks()
+
+    def update(self):
+        # Calculate the time since the last frame was drawn
+        now = pygame.time.get_ticks()
+        elapsed = now - self.last_update
+
+        # If enough time has passed, increment the current frame
+        if elapsed > self.speed:
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+            self.last_update = now
+
+        # Update the rect of the sprite
+        self.rect = self.frames[self.current_frame]
 
     def move(self, adjusted_x, adjusted_y):
         self.x += adjusted_x
@@ -22,5 +52,5 @@ class Character:
             self.move(0, 1)
 
     def draw(self):
-        pygram.draw.rect(screen, (255, 0, 0),
-                         (self.x, self.y, self.width, self.height))
+        surface.blit(self.image, self.rect)
+        #pygame.draw.rect(self.screen, (255, 0, 0),(self.x, self.y, self.width, self.height))
--- main.py
+++ main.py
@@ -15,36 +15,40 @@ class Game:
         self.clock = pygame.time.Clock()
 
     def run(self):
-        # get the currect working directory to locate the spritesheet
+        # get the current working directory to locate the spritesheet
         cwd = os.getcwd()
         # set the path to the directory containing the sprite sheet
-        sprite_dir = os.path.join("c:/Users/long9/OneDrive/Desktop/Spring2023/CPSC 254/proj/Survival-Slayer/assets/")
+        sprite_dir = os.path.join(
+            "c:/Users/long9/OneDrive/Desktop/Spring2023/CPSC 254/proj/Survival-Slayer/assets/")
         # set the path to the sprite sheet file
-        sprite_file = os.path.join(sprite_dir, "character.png")
-        player_spritesheet = pygame.image.load(sprite_file)
-        player = Character(500, 500)  # initializing the player character
+        sprite_file = os.path.join(sprite_dir, "player.png")
+
+        player_spritesheet = pygame.image.load(sprite_file).convert_alpha()
+
+        # initializing the player character
+        player = Character(500, 500, self.screen, sprite_file)
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
-        # check for arrow key presses
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_UP]:
-                player.move_up()
-            elif keys[pygame.K_DOWN]:
-                player.move_down()
-            elif keys[pygame.K_LEFT]:
-                player.move_left()
-            elif keys[pygame.K_RIGHT]:
-                player.move_right()
+                # check for arrow key presses
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_UP]:
+                    player.move_up()
+                elif keys[pygame.K_DOWN]:
+                    player.move_down()
+                elif keys[pygame.K_LEFT]:
+                    player.move_left()
+                elif keys[pygame.K_RIGHT]:
+                    player.move_right()
 
-           # setting up the background and updating the screen
-            self.screen.fill('black')
-            # player.draw(self.screen)
-            pygame.display.update()
-            self.clock.tick(FPS)
+                # setting up the background and updating the screen
+                self.screen.fill('black')
+                player.draw()
+                pygame.display.update()
+                self.clock.tick(FPS)
 
 
 if __name__ == '__main__':
