e75352d4d9299f680706504ab596f11926be0570
fllamas <chewfasa5@hotmail.com>
2023-04-03 20:05:40 -0700

    Added other assets back in to test map changing mechanic with current main branch code.

--- Level.py
+++ Level.py
@@ -20,15 +20,21 @@ class level:
         self.display_surface = pygame.display.get_surface()
         # sprite group set up 
         # self.visibile_sprites = pygame.sprite.Group()
-        self.visibile_sprites = YSortCameraGroup()
+        if current_map == MAP_1:
+            self.visibile_sprites = YSortCameraGroup()
+        else:
+            self.visibile_sprites = YSortCameraGroup_2()
         self.obstacles_sprites = pygame.sprite.Group()
 
-        self.createMap(current_map)    
+        if current_map == MAP_1:
+            self.createMap()    
+        else:
+            self.createMap_2()
 
         # attack sprites
         self.current_attack = None
         #sprite setup
-        self.createMap()    
+        # self.createMap()    
         # user interface
         self.ui = UI()
 
@@ -126,6 +132,71 @@ class level:
             #print(row_index)
             #print(row)
 
+
+    def createMap_2(self):
+        layouts = {
+                'boundary': import_csv_layout('map2/map_FloorBlocks.csv'),
+                'grass': import_csv_layout('map2/map_Grass.csv'),
+                'object': import_csv_layout('map2/map_Objects.csv'), 
+                'entities': import_csv_layout('map2/map_Entities.csv')
+
+        }
+        graphics = {
+                    'grass': import_folder('graphics2/grass'),
+                    'objects': import_folder('graphics2/objects')
+        }
+        # print(graphics)
+        # print(graphics['objects'])
+
+        for style, layout in layouts.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILE_SIZE
+                        y = row_index * TILE_SIZE
+                        if style == 'boundary':
+                            Tile((x, y), [ self.obstacles_sprites], 'invisible')
+                        if style == 'grass':
+                            random_grass_image = choice(graphics['grass'])
+                            Tile((x,y), [self.visibile_sprites, self.obstacles_sprites], 'grass', random_grass_image)
+                        if style == 'object':
+                            surf = graphics['objects'][int(col)] #uses index of the file
+
+                            Tile((x,y), [self.visibile_sprites, self.obstacles_sprites], 'object', surf)
+        #         if col == 'x':
+        #             Tile((x,y), [self.visibile_sprites, self.obstacles_sprites])
+        #         if col == 'p':
+        #             self.player = Character((x,y), [self.visibile_sprites], self.obstacles_sprites)
+        #             # self.visibile_sprites.add(self.player)
+
+                if style == 'entities': 
+                    if col == '394': #el:4:10
+                        self.player = Character(
+                            (x, y),
+                            [self.visibile_sprites],
+                            self.obstacles_sprites,
+                            self.create_attack,
+                            self.destroy_attack,
+                            self.create_magic)
+                    else:
+                        if col == '390': monster_name = 'bamboo'
+                        elif col == '391': monster_name = 'spirit'
+                        elif col == '392': monster_name = 'raccoon'
+                        else: monster_name = 'squid' #this is "working"/running, but need to figure out which numbers insead of 390-392 IF they don't change later
+
+                        Enemy(monster_name, (x,y), [self.visibile_sprites], self.obstacles_sprites)
+    
+        self.player = Character(
+            (500,500),
+            [self.visibile_sprites], 
+            self.obstacles_sprites, 
+            self.create_attack, 
+            self.destroy_attack,
+            self.create_magic)
+            #print(row_index)
+            #print(row)
+
+
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
 
@@ -135,6 +206,7 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
+        #self.current_map = current_map
 
         ##--------------------------    
         # self.scale = 1.0  # default scale
@@ -181,6 +253,43 @@ class YSortCameraGroup(pygame.sprite.Group):
         # scale the floor surface
         #scaled_floor_surface = pygame.transform.scale(self.floor_surface, (int(self.floor_rect.width * self.zoom_level), int(self.floor_rect.height * self.zoom_level)))
     
+
+        #drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surface,floor_offset_pos)
+
+        #for sprite in self.sprites():
+        for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
+
+
+class YSortCameraGroup_2(pygame.sprite.Group):
+    def __init__(self):
+
+        # general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+        #creating the floor, need to change according to picture we decide to use:
+        # self.floor_surface = pygame.image.load('../graphics/tilemap/ground.png').convert()
+        self.floor_surface = pygame.image.load('graphics2/tilemap/ground.png').convert()
+        # self.map_sruface = pygame.Surface((6000, 5000)).convert()
+
+        # self.scaled_map_surface = pygame.transform.scale(self.floor_surface, (2000, 1800))
+
+        #self.floor_surface = pygame.Surface((2000, 1800)).convert()
+
+        self.floor_rect = self.floor_surface.get_rect(topleft = (0,0)) #surf = surface
+
+    def custom_draw(self,player):
+
+        #getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
 
         #drawing the floor
         floor_offset_pos = self.floor_rect.topleft - self.offset
