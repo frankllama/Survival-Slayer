5e1fc56f5d76cde586dbd8ee3bdd2e46eacdc534
fllamas <chewfasa5@hotmail.com>
2023-04-04 23:53:53 -0700

    Added flicker effect when an enemy damages the player and the player also has a breif window of invincibility before they can get hit again.

--- character.py
+++ character.py
@@ -179,6 +179,11 @@ class Character(Entity):
         self.rect = self.image.get_rect(center = self.hitbox.center)
 
         # flicker if the player has been hit
+        if not self.vulnerable:
+            alpha = self.wave_value()
+            self.image.set_alpha(alpha)
+        else:
+            self.image.set_alpha(255)
 
     def get_full_weapon_damage(self):
         base_damage = self.stats['attack']
