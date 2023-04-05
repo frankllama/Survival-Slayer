8ddc437ff05ead3623e9c44ea1cda13f9a86bb9c
fllamas <chewfasa5@hotmail.com>
2023-04-04 23:18:54 -0700

    Added function for enemy reactions to hits. Enemy is now pushed away from player when they are hit with an attack.

--- enemy.py
+++ enemy.py
@@ -124,6 +124,7 @@ class Enemy(Entity):
 
     def get_damage(self, player, attack_type):
         if self.vulnerable:
+            self.direction = self.get_player_distance_direction(player)[1]
             if attack_type == 'weapon':
                 self.health -= player.get_full_weapon_damage()
             else:
@@ -138,7 +139,14 @@ class Enemy(Entity):
             self.kill()
 
 
+    def hit_reaction(self):
+        # enemy will be pushed away in the same facing direction as the player.
+        if not self.vulnerable:
+            self.direction *= -self.resistance
+
+
     def update(self):
+        self.hit_reaction()
         self.move(self.speed)
         self.animate()
         self.checkAttackCooldown()
